07ca440316038c153d592504cb58c8bdec19a095
Jamie Shearer <39914162+Jamie-Shearer@users.noreply.github.com>
2021-02-06 02:22:38 +0000

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -1,3 +1,13 @@
 # This will be the file that simulates the world in which our robots will run
 
 import pybullet as p
+import time
+
+physicsClient = p.connect(p.GUI)    # Creates a physicsClient object, draws result to GUI
+
+
+for i in range(2000):       # Moves time forward in the physics engine by a small amount
+    p.stepSimulation()
+    time.sleep(1/60)
+
+p.disconnect()      # Closes physicsClient
