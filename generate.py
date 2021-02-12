07d79c150fc0c17c6c9a3905a1949e735126ce75
Jamie Shearer <39914162+Jamie-Shearer@users.noreply.github.com>
2021-02-12 20:46:09 +0000

    Add files via upload

--- generate.py
+++ generate.py
@@ -1,4 +1,4 @@
-# Create a single object
+# Create a bunch of objects
 # This then puts an SDF file in the directory containing that object
 
 import pybullet as p
@@ -18,12 +18,28 @@ x = 0
 y = 0
 z = 0.5
 
-# This is what makes the actual box
-for i in range(10):
-    pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
-    z = z + 1
-    length = length * 0.9
-    width = width * 0.9
-    height = height * 0.9
+# A loop that generates towers of boxes in a 6x6 grid
+for k in range(6):              # Each iteration of this creates a row of towers
+    for j in range(6):          # Each iteration of this creates a tower and moves position to next tower
+        for i in range(10):     # Each iteration of this creates the next block and resizes
+            pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])  # Create block
+            z = z + 1               # Set position for next block
+            # Resize next block
+            length = length * 0.9
+            width = width * 0.9
+            height = height * 0.9
+
+        # Set the position to start the next tower
+        x = x + 1   # Move over 1
+        z = 0.5     # It would work without this but it would take forever for all the blocks to fall
+
+        # Resize so the first block of the next tower is the right size
+        length = 1
+        width = 1
+        height = 1
+
+    # Set the starting position of the next row of towers
+    y = y + 1
+    x = 0
 
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,13 +1,20 @@
 # This will be the file that simulates the world in which our robots will run
 
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)    # Creates a physicsClient object, draws result to GUI
+p.setAdditionalSearchPath(pybullet_data.getDataPath())  # Make sure we can find plane.urdf
 
+# Create environment
+p.setGravity(0, 0, -9.8)                # Obviously, sets gravity to simulate the real world
+planeId = p.loadURDF("plane.urdf")      # Make the ground be a flat plane
+p.loadSDF("boxes.sdf")                  # Load in the file created by generate.py
 
-for i in range(2000):       # Moves time forward in the physics engine by a small amount
-    p.stepSimulation()
-    time.sleep(1/60)
+# Keep the environment around for a bit, also walk through time
+for i in range(1000):       # Moves time forward in the physics engine by a small amount
+    p.stepSimulation()      # One time step
+    time.sleep(1/60)        # Keep it around for a bit
 
 p.disconnect()      # Closes physicsClient
