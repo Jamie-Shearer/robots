63846ceaf7b0f53997f9e4b8eb6d8b14480ed326
Jamie Shearer <39914162+Jamie-Shearer@users.noreply.github.com>
2021-02-12 20:12:53 +0000

    Make a tower

--- generate.py
+++ generate.py
@@ -6,14 +6,24 @@ import pyrosim.pyrosim as pyrosim
 
 # This will tell pyrosim where to store info
 # This world will be called box because it will contain a single box
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
 # Initialize variables to dictate size of box
 length = 1
-width = 2
-height = 3
+width = 1
+height = 1
+
+# Initialize position variables
+x = 0
+y = 0
+z = 0.5
 
 # This is what makes the actual box
-pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[length, width, height])
+for i in range(10):
+    pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+    z = z + 1
+    length = length * 0.9
+    width = width * 0.9
+    height = height * 0.9
 
 pyrosim.End()
